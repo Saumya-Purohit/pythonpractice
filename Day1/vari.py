e4754890cbaf59c18e58a8cfd6929f6fad064fb0
Saumya-Purohit <>
2023-02-22 19:07:14 +0530

    Variable interchange exercise

--- Day1/vari.py
+++ Day1/vari.py
@@ -1,6 +1,20 @@
-name="Jack"
-print(name)
+# name="Jack"
+# print(name)
 
-name=input("What is your name ?")
-length=len(name)
-print(length)
+# name=input("What is your name ?")
+# length=len(name)
+# print(length)
+
+a=input("a :")
+b=input("b :")
+
+
+c=a
+a=b
+b=c
+
+
+
+
+print("a= "+a)
+print("b= "+b)
