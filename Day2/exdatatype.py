a40c75f8551249d1f98f2377fd488f51fb555b19
Saumya-Purohit <>
2023-02-23 11:19:16 +0530

    Type conversion practice

---
+++ Day2/exdatatype.py
@@ -0,0 +1,7 @@
+# This is practice for data type
+
+s=str(input("enter a 2 digit number so that we can add it's digit\n"))
+s1=int(s[0])
+s2=int(s[1])
+s3=str(s1+s2)
+print("The sum of digits of number you entered is "+ s3)
