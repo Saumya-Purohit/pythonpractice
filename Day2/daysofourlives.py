20d858cb823bb00a0786065aa1d3e768c45def55
Saumya-Purohit <>
2023-02-23 15:58:22 +0530

    Days of our lives

---
+++ Day2/daysofourlives.py
@@ -0,0 +1,6 @@
+age=int(input("Whats's your current age? \n"))
+lage=90-age
+months=12*age
+weeks=lage*52
+days=lage*365
+print(f"If we assume you live until 90 you have {months} months,{weeks} weeks , {days} days left")
