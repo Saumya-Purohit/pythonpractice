c31a53aef4ea21615b18848848e87fbc262e6f01
Saumya-Purohit <>
2023-02-23 10:31:26 +0530

    Data types

--- Day2/dtypes.py
+++ Day2/dtypes.py
@@ -1,1 +1,18 @@
 print("Hello"[0])
+print("Hello"[4])
+
+# numbers
+
+print(12345)
+print(123_456_789)
+
+#float
+
+print(3.2672792)
+print(3.33_33_36286_78)
+
+
+#boolean
+
+print(True)
+print(False)
