bce0d9199049a40ea8ad71e0a86522b7091e750f
Saumya-Purohit <>
2023-03-05 18:51:23 +0530

    Learning to use elif statement

--- Day3/roll.py
+++ Day3/roll.py
@@ -1,6 +1,13 @@
 print ("Welcome to the rollercoaster!")
 height =int(input("What isvyour height in cm?"))
 if(height>=120):
-    print("Yayy!! You can ride the rollercoster")
+    print("Yayy!! You can ride the rollercoster \n")
+    age=int(input("Enter your age please ! : "))
+    if(age<=5):
+        print("Your fare is rs. 7")
+    elif(5<age<=12):
+        print("Your fare is 10")
+    else:
+        print("Your fare is 15")
 else:
     print("Aww!! Sorry bud better luck next time")
