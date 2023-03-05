5be607ced6c960324cb568808745dec54ec7c005
Saumya-Purohit <>
2023-03-05 18:29:40 +0530

    Roller checker

---
+++ Day3/roll.py
@@ -0,0 +1,6 @@
+print ("Welcome to the rollercoaster!")
+height =int(input("What isvyour height in cm?"))
+if(height>=120):
+    print("Yayy!! You can ride the rollercoster")
+else:
+    print("Aww!! Sorry bud better luck next time")
