ca52849afcb0f0a4cbfb15b692263fdc041e4720
Saumya-Purohit <>
2023-03-05 18:36:17 +0530

    oddeven check

---
+++ Day3/oddevencheck.py
@@ -0,0 +1,8 @@
+no=int(input("Enter a no. you want to check for even odd \n"))
+if(no==0):
+    print("It's 0 enter something else !")
+else:
+    if(no%2==0):
+        print("Heya! It's an even number") 
+    else:
+        print("It's an odd number bud ")
